1c940ee4b8ad654a58c1c6821f5e432e64c4ce39
JulianJage <julian.jage@gmx.net>
2018-03-18 16:56:39 +0100

    added comments (motor, colorsensor, linefollower)

--- src/sensors/colorsensor.py
+++ src/sensors/colorsensor.py
@@ -1,29 +1,36 @@
 import ev3dev.ev3 as ev3
-
 import time
 
 
 class ColorSensor:
 
+    # constructor: setup input for ColorSensor and set mode to RGB-RAW
     def __init__(self):
         self.colorS = ev3.ColorSensor('in2')
         self.colorS.mode = 'RGB-RAW'
 
+    # set the color mode
+    # 1 parameter -> new_mode (string value): set new color mode
     def set_mode(self, new_mode):
         self.colorS.mode = new_mode
 
+    # return the current detected color value as rgb-triplet
+    # as tuple in format: (red, green, blue)
     def get_value(self):
-        self.colorS.mode = 'RGB-RAW'
         print(self.colorS.bin_data())
-        return self.colorS.bin_data()
+        return self.colorS.bin_data("hhh")
 
+    # returns the current brightness value disregarding specific colors
     def get_avg(self):
         rgb = self.colorS.bin_data("hhh")
         avg = (rgb[0] + rgb[1] + rgb[2])//3
         return avg
 
+    # test-function to print 1000 detected RGB-values
     def print_values(self):
-        for i in range(0,1000):
+        # loops 1000 times
+        for i in range(0, 1000):
             self.colorS.mode = 'RGB-RAW'
             print(self.colorS.bin_data("hhh"))
+            # pauses for 100ms
             time.sleep(.1)
--- src/sensors/motor.py
+++ src/sensors/motor.py
@@ -3,15 +3,21 @@ import ev3dev.ev3 as ev3
 
 class Motor:
 
+    # constructor: setup corresponding EV3 outputs
     def __init__(self):
         self.motor_left = ev3.LargeMotor('outA')
         self.motor_right = ev3.LargeMotor('outD')
+        # slow preset speed
         self.speed = 100
 
+    # driving forward for a specific time
+    # 1 parameter -> time: how long to drive in ms
     def forward(self, time):
         self.motor_left.run_timed(time_sp=time, speed_sp=self.speed)
         self.motor_right.run_timed(time_sp=time, speed_sp=self.speed)
 
+    # drive with separate speeds for left and right wheel to allow turns
+    # 3 parameters -> time: in ms, speed_left: speed for left wheel, speed_right: speed for right wheel
     def move_left_right(self, time, speed_left, speed_right):
         self.motor_left.run_timed(time_sp=time, speed_sp=speed_left)
         self.motor_right.run_timed(time_sp=time, speed_sp=speed_right)

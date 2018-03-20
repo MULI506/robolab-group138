b58f64b26474533981ceb5e1a4a04c79973537d1
JulianJage <julian.jage@gmx.net>
2018-03-20 17:11:58 +0100

    implemented functional odometry module

--- src/odometry.py
+++ src/odometry.py
@@ -36,8 +36,45 @@ class Odometry():
     def limit_rotation_degree(self, rot_deg):
         return rot_deg % 360
 
+    def guess_direction(self, rotation):
+        if 0 <= rotation < 45 or rotation >= 315:
+            return 'NORTH'
+        elif 45 <= rotation < 135:
+            return 'WEST'
+        elif 135 <= rotation < 225:
+            return 'SOUTH'
+        elif 225 <= rotation < 315:
+            return 'EAST'
+        else:
+            return 'ERROR'
+
+    # guess the new coordinate and rotation/direction of the robot
+    def guess_position(self, coordinate_old):
+        # COORDINATE
+        # distance between nodes in mm
+        grid_size = 500
+        x_change_grid = round(self.current_position[0] / grid_size)
+        y_change_grid = round(self.current_position[1] / grid_size)
+
+        print("x_change: {}, y_change: {}".format(x_change_grid, y_change_grid))
+
+        # coordinate changes added to old coordinates
+        x_guess = coordinate_old[0] + x_change_grid
+        y_guess = coordinate_old[1] + y_change_grid
+        # new coordinates as tuple
+        coordinate_guess = (x_guess, y_guess)
+        print("old_c: {}, guess_c: {}".format(coordinate_old, coordinate_guess))
+
+        # ROTATION
+        rot_guess = self.guess_direction(self.current_rotation)
+        print("rotation: measured {}, guessed {}".format(int(self.current_rotation), rot_guess))
+
+        position_guess = (coordinate_guess, rot_guess)
+        return position_guess
+
+
     # calculate change in positino and rotation according to measured wheel rotation
-    def calculate_new_position(self, wheel_position, position_robot_old, rotation_old, axis):
+    def calculate_new_position(self, wheel_position):
         # degrees that each wheel moved
         steps_left = wheel_position[0]
         steps_right = wheel_position[1]
@@ -45,18 +82,23 @@ class Odometry():
         # moved distance / displacement
         distance = (steps_left + steps_right) * self.step_distance / 2
         # difference in rotation
-        rotation_diff = (steps_right - steps_left) * self.step_distance / axis
+        rotation_diff = (steps_right - steps_left) * self.step_distance / self.wheel_axis
         # new total rotation direction
-        rotation_new = rotation_old + rotation_diff
+        rotation_new = math.radians(self.current_rotation) + rotation_diff
         # test print-functions
-        #print("rot. old:{}, diff:{}, new:{}".format(int(math.degrees(rotation_old)), int(math.degrees(rotation_diff)), int(math.degrees(rotation_new))))
+        #print("rot. old:{}, diff:{}, new:{}".format(self.current_rotation, int(math.degrees(rotation_diff)), int(math.degrees(rotation_new))))
         #print("cos_x: {}, sin_x: {}".format(distance * math.cos(rotation_new), distance * math.sin(rotation_new)))
         # new x,y coordinates
-        pos_x = position_robot_old[0] + distance * math.cos(rotation_new)
-        pos_y = position_robot_old[1] + distance * math.sin(rotation_new)
+        pos_x = self.current_position[0] + distance * math.cos(rotation_new)
+        pos_y = self.current_position[1] + distance * math.sin(rotation_new)
 
         position_new = (pos_x, pos_y)
-        pos_rot_value = (position_new, self.limit_rotation_degree(math.degrees(rotation_new)))
+        rotation_new_degree = self.limit_rotation_degree(math.degrees(rotation_new))
+        pos_rot_value = (position_new, rotation_new_degree)
+
+        self.current_rotation = rotation_new_degree
+        self.current_position = position_new
+        #print("rotation_new: {}, position_new: {}, self_rotation: {}, self_position: {}".format(rotation_new, position_new, self.current_rotation, self.current_position))
         return pos_rot_value
 
 
